0eeafb7c5dc562dfd1c11056decdd407c4fc75a5
Heliodex <heli@odex.cf>
2025-10-30 12:05:22 +0000

    Form handling, more customisable styling with open props

--- ecommerce/lib/Page.php
+++ ecommerce/lib/Page.php
@@ -20,9 +20,6 @@ class Page
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link
-		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css">
 	<link rel="stylesheet" href="/static/main.css">
 	<title>{$this->title}</title>
 </head>
--- ecommerce/login.php
+++ ecommerce/login.php
@@ -2,6 +2,27 @@
 
 declare(strict_types=1);
 
+$errors = [];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$username = $_POST["username"] ?? "";
+	$password = $_POST["password"] ?? "";
+
+	if (empty($username))
+		$errors["username"] = "Username is required";
+	elseif (strlen($username) < 3)
+		$errors["username"] = "Username must be at least 3 characters long";
+
+	if (empty($password))
+		$errors["password"] = "Password is required";
+	elseif (strlen($password) < 3)
+		$errors["password"] = "Password must be at least 3 characters long";
+
+	if (empty($errors)) {
+		echo "Processing login...";
+	}
+}
+
 require_once "lib/page.php";
 
 $_ = new Page("Log in");
@@ -9,14 +30,26 @@ $_ = new Page("Log in");
 
 <h1>Log in</h1>
 
-<form>
+<form method="post">
 	<fieldset>
-		<label for="username">Username:</label>
+		<label for="username">Username</label>
 		<input type="text" id="username" name="username" required>
-		<br />
-		<label for="password">Password:</label>
-		<input type="password" id="password" name="password" required>
-		<br />
-		<input type="submit" value="Login">
+		<?php
+		$eusername = $errors["username"] ?? null; // bruh
+		if (isset($eusername))
+			echo "<small class=\"formerror\">$eusername</small>";
+		?>
 	</fieldset>
+
+	<fieldset>
+		<label for="password">Password</label>
+		<input type="password" id="password" name="password" required>
+		<?php
+		$epassword = $errors["password"] ?? null;
+		if (isset($epassword))
+			echo "<small class=\"formerror\">$epassword</small>";
+		?>
+	</fieldset>
+
+	<button>Log in</button>
 </form>
