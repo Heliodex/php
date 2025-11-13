2aaf29293e40c4f5fd253c12444305fdd8e8cadc
Heliodex <heli@odex.cf>
2025-11-13 12:52:12 +0000

    Boost site with htmx
    hype

--- ecommerce/lib/page.php
+++ ecommerce/lib/page.php
@@ -37,9 +37,10 @@ class Page
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="/static/main.css">
 	<title>{$this->title}</title>
+	<script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.8/dist/htmx.min.js" integrity="sha384-/TgkGk7p307TH7EXJDuUlgG3Ce1UVolAOFopFekQkkXihi5u/6OCvVKyz1W+idaz" crossorigin="anonymous"></script>
 </head>
 
-<body>
+<body hx-boost="true">
 	<header>
 		<nav><ul>{$links}</ul></nav>
 	</header>
