3dcb9d49ee2ed9fa629bba12df68c3a2132c27e5
Lewin Kelly <heli@odex.cf>
2026-02-10 09:08:11 +0000

    Remove demo application

--- demo/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Kernel;
-
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
-
-return fn(array $context) =>
-	new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-
